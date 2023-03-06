819f0e7c4ab34a0883ffd8900e2c2efff40b9e40
Raja Salomo Sitompul <raja.sitompul@adira.co.id>
2023-03-06 17:32:42 +0700

    Berhasil reverse proxy

--- controllers/app_c.go
+++ controllers/app_c.go
@@ -1,8 +1,15 @@
 package controllers
+
 import (
-	"github.com/joho/godotenv"
 	"context"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -23,17 +30,17 @@ func init() {
 	REDIS_USER = os.Getenv("REDIS_USER")
 	REDIS_PASS = os.Getenv("REDIS_PASS")
 	REDIS_PORT = os.Getenv("REDIS_PORT")
-	opt, _ := redis.ParseURL("redis://"+REDIS_USER+":"+REDIS_PASS+"@"+REDIS_HOST_CLOUD+":"+REDIS_PORT)
+	opt, _ := redis.ParseURL("redis://" + REDIS_USER + ":" + REDIS_PASS + "@" + REDIS_HOST_CLOUD + ":" + REDIS_PORT)
 	rdb = redis.NewClient(opt)
 }
-func SaveRedis(key string, data string) (string,string){
+func SaveRedis(key string, data string) (string, string) {
 	err := rdb.Set(ctx, key, data, 0).Err()
 	if err != nil {
 		return `{"ok":true}`, `{"error":"` + err.Error() + `"}`
 	}
 	return `{"ok":true}`, ""
 }
-func GetRedis(key string) string{
+func GetRedis(key string) string {
 	if ERROR_LOAD_ENV != "" {
 		return `{"error":"` + ERROR_LOAD_ENV + `"}`
 	}
@@ -42,4 +49,31 @@ func GetRedis(key string) string{
 		return `{"error":"` + err.Error() + `"}`
 	}
 	return val
+}
+func ForwardRequest(service string, c *gin.Context) {
+	if service == "" {
+		panic("Service tidak dikenali")
+	}
+	remote, err := url.Parse("http://localhost:7890/")
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	// log.Println(proxy)
+	// //Define the director func
+	// //This is a good place to log, for example
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Request.URL.String()
+		// Disini tempat untuk save LOG
+	}
+
+	proxy.ModifyResponse = func(resp *http.Response) error {
+		log.Println("xxxx", resp)
+		return nil
+	}
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	_ "log"
-	"wmsgateway/routers"
+	"wmsgateway/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	routers.GatewayRouter(r)
+	r.Any("/:service/*path", func(c *gin.Context) {
+		controllers.ForwardRequest(c.Param("service"), c)
+	})
+	// r.Any("/*proxyPath", controllers.ForwardRequest)
+
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(404, gin.H{"code": "404", "message": "Page not found"})
 	})
